07dd2248674cbde5f9cbc6c7af3acf0ba3b63741
Álvaro <vegaromeroalvaro@gmail.com>
2023-03-29 20:44:22 +0200

    sorting comments available

--- Homeworks/1st homework/scripts.js
+++ Homeworks/1st homework/scripts.js
@@ -13,8 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function addOldComments(){
+    
     const entries = Object.entries(localStorage)
     const maxKey = getMaximumKey(entries)
+
+    const comments = document.getElementById("comments")
+    const children_size = comments.children.length
+
+    //if this happen, its because it's because we want to sort it by time
+    if(children_size > 4){ //4 first children should be always there  
+
+        //We remove the unordered comments    
+        for(let i = children_size-1; i >= 4 ; i--){
+          //console.log(comments.children[i+3])
+          comments.removeChild(comments.children[i])
+        }
+
+        document.getElementById("sort_time_button").hidden = true;
+        document.getElementById("sort_alph_button").hidden = false;
+    }
 
     if(entries.length > 0){
         for(let i = 0; i <= maxKey; i++){ //values should be under maxKey
@@ -106,24 +123,55 @@ function domRemoveComment(html_comment) {
 function domSortAlphabeticallyComments(){ //Alphabetically in function of the author
     var comments = document.getElementById("comments")
 
-    while (comments.firstChild) {
-        comments.removeChild(comments.firstChild);
-    }
-    
-    //Create an array of the names, and sort them alphabetically
     entries = Object.entries(localStorage);
-    const names = [];
+    const maxKey = getMaximumKey(entries)
 
     if(entries.length > 0){
-        for(let i = 0; i < entries; i++){
+
+        //We remove the unordered comments    
+        for(let i = entries.length-1; i >= 0 ; i--){
+            //console.log(comments.children[i+3])
+            comments.removeChild(comments.children[i+4])
+        }
+
+        //Create an array of the names, and sort them alphabetically
+        const names = [];
+
+        for(let i = 0; i <= maxKey; i++){
             let string = localStorage.getItem(i)
             let json = JSON.parse(string)
 
-            names.push(json.name);
+            if(json != null)
+                names.push([json.name, json.key]);
+        }
+        
+        names.sort(function(a, b) { //sort the array
+            if (a[0] < b[0]) {
+              return -1;
+            } else if (a[0] > b[0]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+
+        //we have an array with the names sorted and its key
+        
+        //we add the comments ordered
+        for(let i = 0; i < names.length; i++){
+            let string = localStorage.getItem(names[i][1])
+            let json = JSON.parse(string)
+            
+            let comment = {
+                name: json.name,
+                opinion: json.opinion,
+                key: json.key
+            };
+
+            domAddComment(comment)
         }
 
-        names.sort()
-        console.log(names)
+        document.getElementById("sort_time_button").hidden = false;
+        document.getElementById("sort_alph_button").hidden = true;
     }
-
 }
